d2051dcf808d317eaf4058efdce4544cb8e73693
ahmadssadiq <91856553+ahmadssadiq@users.noreply.github.com>
2024-10-15 13:56:42 -0400

    wired the urls.py to the filesl, created the html form in templates and the views.py to incldue the html form file

--- mini_fb/urls.py
+++ mini_fb/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from .views import ShowAllProfilesView
+from .views import ShowAllProfilesView, ShowProfilePageView, CreateProfileView
 
 urlpatterns = [
+    # URL for showing all profiles
     path("", ShowAllProfilesView.as_view(), name="show_all_profiles"),
+    # URL for showing a specific profile by primary key
+    path("profile/<int:pk>/", ShowProfilePageView.as_view(), name="show_profile"),
+    # URL for creating a new profile
+    path("create_profile/", CreateProfileView.as_view(), name="create_profile"),
 ]
-    
--- mini_fb/views.py
+++ mini_fb/views.py
@@ -1,8 +1,15 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Profile
 
 
+# View to show all profiles
 class ShowAllProfilesView(ListView):
     model = Profile
     template_name = "mini_fb/show_all_profiles.html"
     context_object_name = "profiles"
+
+
+# View to show a single profile's page
+class ShowProfilePageView(DetailView):
+    model = Profile
+    template_name = "mini_fb/show_profile.html"
